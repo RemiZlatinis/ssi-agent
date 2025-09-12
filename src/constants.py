0fd1ecf1f6424b5e6431483228746e56b0142bf2
RemiZlatinis <remizlatinis@gmail.com>
2025-09-12 13:56:57 +0300

    Fix paths against Linux FHS

--- src/constants.py
+++ src/constants.py
@@ -3,6 +3,6 @@
 from pathlib import Path
 
 PREFIX = "ssi_"  # Prefix for systemd services
-SERVICES_DIR = Path("/usr/lib/systemd/system")
-SCRIPTS_DIR = Path("/usr/local/bin/service-status-indicator")
+SERVICES_DIR = Path("/usr/local/lib/systemd/system/")
+SCRIPTS_DIR = Path("/usr/local/lib/service-status-indicator/")
 LOG_DIR = Path("/var/log/service-status-indicator")
